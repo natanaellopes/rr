c2e14e4d8c771946c40cf84a87d2588ce63fc6f6
Natanael Lopes <natanael.lopes.cruz@gmail.com>
2017-10-09 20:52:52 -0300

    Email settings

--- send.php
+++ send.php
@@ -1,3 +1,13 @@
 <?php
 
-return true;
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+
+$html = "Contato enviado pelo site" . "\n";
+$html .= "Nome: $name" . "\n";
+$html .= "Email: $email" . "\n";
+$html .= "Mensagem: $message" . "\n";
+$html .= "Enviado em " . date('Y-m-d H:i:s');
+
+mail('natanael5889@gmail.com', 'Contato enviado pelo site', $html);
